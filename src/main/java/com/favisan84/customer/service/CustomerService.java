8e9ef2790192b4ea913e0abb89e3f5f154b81e68
unknown <FABIO.DOSSANTOS@CORP.GSOFTWARE.COM.BR>
2021-12-16 14:56:32 -0300

    Configure sonar workflow

--- src/main/java/com/favisan84/customer/service/CustomerService.java
+++ src/main/java/com/favisan84/customer/service/CustomerService.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.validation.ValidationException;
-import java.util.Optional;
 
 @Service
 public class CustomerService {
